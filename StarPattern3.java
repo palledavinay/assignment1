473c3f60b45f71e30b04182c5747ff64d51a5d12
PALLEDA VINAY KUMAR <101565026+palledavinay@users.noreply.github.com>
2022-10-23 18:28:36 +0530

    Add files via upload

---
+++ StarPattern3.java
@@ -0,0 +1,23 @@
+package vinay;
+
+public class StarPattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=14;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(i==n-1||(i-j>=(n-1)/2&&j<=(n-1)/2&&i>=(n-1)/2)||i==0||(i+j<=(n-1)/2&&i<=(n-1)/2&&j<=(n-1)/2)) {
+					System.out.print("*");
+					
+				}
+				else {
+					System.out.print(" ");
+					
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
